c8a946ddb033a66573cb4f785a7c5836d22289f9
Ken Fowler <ken@strata.ltd>
2023-02-01 18:46:32 -0500

    feat: add token types used by language

--- src/TokenType.ts
+++ src/TokenType.ts
@@ -1,9 +1,52 @@
 enum TokenType {
-  // single character
-
-  // arithmetic operators
+  // single character tokens
+  LEFT_PAREN,
+  RIGHT_PAREN,
+  LEFT_BRACE,
+  RIGHT_BRACE,
+  COMMA,
+  DOT,
   PLUS,
   MINUS,
+  SEMICOLON,
+  SLASH,
+  STAR,
+
+  // one or two character tokens
+  BANG,
+  BANG_EQAUL,
+  EQUAL,
+  EQUAL_EQUAL,
+  GREATER,
+  GREATER_EQUAL,
+  LESS,
+  LESS_EQUAL,
+
+  // literals
+  IDENTIFIER,
+  STRING,
+  NUMBER,
+
+  // keywords
+  AND,
+  CLASS,
+  ELSE,
+  FALSE,
+  FUN,
+  FOR,
+  IF,
+  NIL,
+  OR,
+  PRINT,
+  RETURN,
+  SUPER,
+  THIS,
+  TRUE,
+  VAR,
+  WHILE,
+
+  //
+  EOF,
 }
 
 export default TokenType;
