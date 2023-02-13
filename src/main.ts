f4dfbaed928f19a501d2d39b3eda0c49783d3165
Ken Fowler <development@kennyf.ca>
2023-02-12 19:25:39 -0500

    feat: run lox with file path and REPL

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,27 @@
-import path from "path";
-import fs from "fs";
+import path from 'path';
+import fs from 'fs';
+import Lox from './Lox';
 
+// we want to drop the user into a REPL for our interpreter if there is no file path provided.
 function main() {
+  const lox = new Lox();
   const filePath = process.argv[2];
 
   if (!filePath) {
-    console.error("Error: No file path provided");
-    process.exit(1);
+    lox.runPrompt();
   }
 
   const fullPath = path.resolve(process.cwd(), filePath);
 
-  fs.readFile(fullPath, "utf8", (err, data) => {
+  fs.readFile(fullPath, 'utf8', (err, data) => {
     if (err) {
       console.error(`Error: Unable to read file at path ${fullPath}`);
       process.exit(1);
     }
 
-    console.log(`Contents of file at path ${fullPath}:`);
-    console.log(data);
+    console.log(`Running tslox interpreter on file -> ${fullPath}:`);
+    console.log(lox.run(data));
+    process.exit(0);
   });
 }
 
