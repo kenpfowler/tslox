8f669325cf1bd7ba74030e2eeba13b3389b9fa7a
Ken Fowler <development@kennyf.ca>
2023-02-12 23:56:23 -0500

    feat: added feeback for users entering program

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import Lox from './Lox';
 
 // we want to drop the user into a REPL for our interpreter if there is no file path provided.
 function main() {
+  const tooManyArgs = process.argv.length > 3;
   const filePath = process.argv[2];
+
+  if (tooManyArgs) {
+    console.log('Usage: tlox [script]');
+    process.exit(1);
+  }
 
   if (!filePath) {
     Lox.runPrompt();
