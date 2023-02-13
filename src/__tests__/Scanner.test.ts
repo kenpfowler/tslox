eaad99d37cc56f377d5a256daeac5423138888dc
Ken Fowler <development@kennyf.ca>
2023-02-12 20:58:07 -0500

    feat: add test for accuracy of token emission

--- src/__tests__/Scanner.test.ts
+++ src/__tests__/Scanner.test.ts
@@ -1,10 +1,39 @@
-import TokenType from '../TokenType';
-import Token from '../Token';
 import Scanner from '../Scanner';
+import TokenType from '../TokenType';
 
 describe('Test Scanner Implimentation', () => {
-  const scanner = new Scanner('(){};,+-*!===<=>=!=<>/.');
+  let current = 0;
+  const source = '(){};,+-*!===<=>=!=<>/.';
+  const scanner = new Scanner(source);
+  const tokens = scanner.scanForTokens();
+  const expected = [
+    TokenType.LEFT_PAREN,
+    TokenType.RIGHT_PAREN,
+    TokenType.LEFT_BRACE,
+    TokenType.RIGHT_BRACE,
+    TokenType.SEMICOLON,
+    TokenType.COMMA,
+    TokenType.PLUS,
+    TokenType.MINUS,
+    TokenType.STAR,
+    TokenType.BANG_EQAUL,
+    TokenType.EQUAL_EQUAL,
+    TokenType.LESS_EQUAL,
+    TokenType.GREATER_EQUAL,
+    TokenType.BANG_EQAUL,
+    TokenType.LESS,
+    TokenType.GREATER,
+    TokenType.SLASH,
+    TokenType.DOT,
+    TokenType.EOF,
+  ];
+
   it('It should return 19 tokens', () => {
-    expect(scanner.scanForTokens().length).toBe(19);
+    expect(tokens.length).toBe(19);
+  });
+
+  test.each(tokens)('Token should be identified as: %s', ({ type }) => {
+    expect(type).toBe(expected[current]);
+    current++;
   });
 });
