a066fb4a2f69a749e5161b14f2b42c946a702190
Ken Fowler <development@kennyf.ca>
2023-02-12 23:56:38 -0500

    feat: added testing

--- src/__tests__/Scanner.test.ts
+++ src/__tests__/Scanner.test.ts
@@ -1,7 +1,8 @@
 import Scanner from '../Scanner';
 import TokenType from '../TokenType';
+import Token from '../Token';
 
-describe('Test Scanner Implimentation', () => {
+describe('Test Scanner Implimentation: Punctuators', () => {
   let current = 0;
   const source = '(){};,+-*!===<=>=!=<>/.';
   const scanner = new Scanner(source);
@@ -27,12 +28,121 @@ describe('Test Scanner Implimentation', () => {
     TokenType.DOT,
     TokenType.EOF,
   ];
+  const total = expected.length;
 
-  it('It should return 19 tokens', () => {
-    expect(tokens.length).toBe(19);
+  it(`It should return ${total} tokens`, () => {
+    expect(tokens.length).toBe(total);
   });
 
-  test.each(tokens)('Token should be identified as: %s', ({ type }) => {
+  test.each<Token>(tokens)('Token should be identified as: $type', ({ type }) => {
+    expect(type).toBe(expected[current]);
+    current++;
+  });
+});
+
+describe('Test Scanner Implimentation: Keywords', () => {
+  let current = 0;
+  const source = 'and class else false for fun if nil or return super this true var while';
+  const scanner = new Scanner(source);
+  const tokens = scanner.scanForTokens();
+  const expected = [
+    TokenType.AND,
+    TokenType.CLASS,
+    TokenType.ELSE,
+    TokenType.FALSE,
+    TokenType.FOR,
+    TokenType.FUN,
+    TokenType.IF,
+    TokenType.NIL,
+    TokenType.OR,
+    TokenType.RETURN,
+    TokenType.SUPER,
+    TokenType.THIS,
+    TokenType.TRUE,
+    TokenType.VAR,
+    TokenType.WHILE,
+    TokenType.EOF,
+  ];
+  const total = expected.length;
+
+  it(`It should return ${total} tokens`, () => {
+    expect(tokens.length).toBe(total);
+  });
+
+  test.each<Token>(tokens)('Token should be identified as: $type', ({ type }) => {
+    expect(type).toBe(expected[current]);
+    current++;
+  });
+});
+
+describe('Test Scanner Implimentation: Identifiers', () => {
+  let current = 0;
+  const source =
+    'andy formless fo _ _123 _abc ab123 abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_';
+  const scanner = new Scanner(source);
+  const tokens = scanner.scanForTokens();
+  const expected = [
+    TokenType.IDENTIFIER,
+    TokenType.IDENTIFIER,
+    TokenType.IDENTIFIER,
+    TokenType.IDENTIFIER,
+    TokenType.IDENTIFIER,
+    TokenType.IDENTIFIER,
+    TokenType.IDENTIFIER,
+    TokenType.IDENTIFIER,
+    TokenType.EOF,
+  ];
+  const total = expected.length;
+
+  it(`It should return ${total} tokens`, () => {
+    expect(tokens.length).toBe(total);
+  });
+
+  test.each<Token>(tokens)('Token should be identified as: $type', ({ type }) => {
+    expect(type).toBe(expected[current]);
+    current++;
+  });
+});
+
+describe('Test Scanner Implimentation: Strings', () => {
+  let current = 0;
+  const source = '"""string"';
+  const scanner = new Scanner(source);
+  const tokens = scanner.scanForTokens();
+  const expected = [TokenType.STRING, TokenType.STRING, TokenType.EOF];
+  const total = expected.length;
+
+  it(`It should return ${total} tokens`, () => {
+    expect(tokens.length).toBe(total);
+  });
+
+  test.each<Token>(tokens)('Token should be identified as: $type', ({ type }) => {
+    expect(type).toBe(expected[current]);
+    current++;
+  });
+});
+
+describe('Test Scanner Implimentation: Numbers', () => {
+  let current = 0;
+  const source = '123 123.456 .456 123.';
+  const scanner = new Scanner(source);
+  const tokens = scanner.scanForTokens();
+  const expected = [
+    TokenType.NUMBER,
+    TokenType.NUMBER,
+    TokenType.DOT,
+    TokenType.NUMBER,
+    TokenType.NUMBER,
+    TokenType.DOT,
+    TokenType.EOF,
+  ];
+  const total = expected.length;
+
+  it(`It should return ${total} tokens`, () => {
+    expect(tokens.length).toBe(total);
+  });
+
+  test.each<Token>(tokens)('Token should be identified as: $type', ({ type }) => {
     expect(type).toBe(expected[current]);
     current++;
   });
