1d9e6fd52bc43bd4f8893c49f02b6ad58f40a905
Ken Fowler <development@kennyf.ca>
2023-02-15 22:21:13 -0500

    feat: create expression classes

--- src/Expression.ts
+++ src/Expression.ts
@@ -1,6 +1,72 @@
-abstract class Expression {}
+import Token from './Token';
 
-class Binary extends Expression {}
-class Grouping extends Expression {}
-class Unary extends Expression {}
-class Literal extends Expression {}
+/*
+These Expression classes are meant to represent our syntax tree structures.
+They don't do anything. They are just data structures to represent valid grouping.
+What is done with them will be up to the classes that use them.
+The Parser will create these trees and the Interpreter class will consume them.
+They are bridge for the Parser and Interpreter classes to communicate.
+*/
+interface Visitor<R> {
+  visitBinaryExpression: (expression: Binary) => R;
+  visitGroupingExpression: (expression: Grouping) => R;
+  visitLiteralExpression: (expression: Literal) => R;
+  visitUnaryExpression: (expression: Unary) => R;
+}
+
+abstract class Expression {
+  abstract accept<R>(visitor: Visitor<R>): R;
+}
+
+export class Binary extends Expression {
+  readonly left: Expression;
+  readonly operator: Token;
+  readonly right: Expression;
+
+  constructor(left: Expression, operator: Token, right: Expression) {
+    super();
+    this.left = left;
+    this.operator = operator;
+    this.right = right;
+  }
+
+  public accept<R>(visitor: Visitor<R>): R {
+    return visitor.visitBinaryExpression(this);
+  }
+}
+
+export class Grouping extends Expression {
+  readonly expression: Expression;
+  constructor(expression: Expression) {
+    super();
+    this.expression = expression;
+  }
+
+  public accept<R>(visitor: Visitor<R>): R {
+    return visitor.visitGroupingExpression(this);
+  }
+}
+export class Unary extends Expression {
+  readonly operator: Token;
+  readonly expression: Expression;
+  constructor(token: Token, expression: Expression) {
+    super();
+    this.operator = token;
+    this.expression = expression;
+  }
+
+  public accept<R>(visitor: Visitor<R>): R {
+    return visitor.visitUnaryExpression(this);
+  }
+}
+export class Literal extends Expression {
+  readonly value: object;
+  constructor(value: object) {
+    super();
+    this.value = value;
+  }
+
+  public accept<R>(visitor: Visitor<R>): R {
+    return visitor.visitLiteralExpression(this);
+  }
+}
