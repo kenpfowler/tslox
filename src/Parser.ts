49762f80ea8c9995b4b2a4d9571c019ba3e42a69
Ken Fowler <development@kennyf.ca>
2023-07-31 08:00:16 -0400

    feat: parse statements

--- src/Parser.ts
+++ src/Parser.ts
@@ -1,7 +1,7 @@
-import { Binary, Expression, Grouping, Literal, Unary } from './Expression';
+import { Binary, Expr, Grouping, Literal, Unary, Variable } from './Expr';
 import Lox from './Lox';
 import ParseError from './ParseError';
-import { ExpressionStatement, PrintStatement, Statement } from './Statement';
+import { ExpressionStatement, Print, Stmt, Var } from './Stmt';
 import Token from './Token';
 import TokenType from './TokenType';
 
@@ -19,12 +19,43 @@ class Parser {
   }
 
   parse() {
-    const statements = new Array<Statement>();
+    const statements = new Array<Stmt>();
     while (!this.isAtEnd()) {
-      statements.push(this.statement());
+      statements.push(this.declaration());
     }
 
     return statements;
+  }
+
+  private declaration() {
+    try {
+      if (this.match(TokenType.VAR)) return this.varDeclaration();
+
+      return this.statement();
+    } catch (error) {
+      if (error instanceof ParseError) {
+        this.synchronize();
+        // FIXME: this is a bandaid. Apperently typed java objects can be initialized as null and still be passed around as the type they are labeled as.
+        // This doesn't work in typescript.  Our code in parse on line 24 expects the declaration method to return a statement.
+        return null as any;
+      }
+    }
+  }
+
+  private varDeclaration() {
+    const name = this.consume(TokenType.IDENTIFIER, 'Expect variable name.');
+
+    let initializer = null;
+
+    if (this.match(TokenType.EQUAL)) {
+      initializer = this.expression();
+    }
+
+    this.consume(TokenType.SEMICOLON, "Expect ';' after variable declaration.");
+
+    // FIXME: this is a bandaid. Apperently typed java objects can be initialized as null and still be passed around as the type they are labeled as.
+    // This doesn't work in typescript.  Our constructor expects an expression but the value returned is possibly null.
+    return new Var(name, initializer as Expr);
   }
 
   private statement() {
@@ -35,13 +66,13 @@ class Parser {
   private printStatement() {
     const value = this.expression();
     this.consume(TokenType.SEMICOLON, "Expect ';' after value.");
-    return new PrintStatement(value);
+    return new Print(value);
   }
 
   private expressionStatement() {
-    const expression = this.expression();
+    const value = this.expression();
     this.consume(TokenType.SEMICOLON, "Expect ';' after expression.");
-    return new ExpressionStatement(expression);
+    return new ExpressionStatement(value);
   }
 
   private expression() {
@@ -102,10 +133,11 @@ class Parser {
     return expr;
   }
 
-  private unary(): Expression {
+  private unary(): Expr {
     if (this.match(TokenType.BANG, TokenType.MINUS)) {
       const operator = this.previous();
       const right = this.unary();
+
       return new Unary(operator, right);
     }
 
@@ -121,18 +153,22 @@ class Parser {
       return new Literal(this.previous().literal);
     }
 
+    if (this.match(TokenType.IDENTIFIER)) {
+      return new Variable(this.previous());
+    }
+
     if (this.match(TokenType.LEFT_PAREN)) {
       const expr = this.expression();
       this.consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.");
       return new Grouping(expr);
     }
 
+    // FIXME: this is where the error is thrown if a print stmt is called without argument/expression. can we spy on this?
     throw this.error(this.peek(), 'Expect expression.');
   }
 
   private consume(type: TokenType, message: string) {
     if (this.check(type)) return this.advance();
-
     throw new Error(message);
   }
 
@@ -143,7 +179,7 @@ class Parser {
 
   private check(type: TokenType) {
     if (this.isAtEnd()) return false;
-    return this.peek().type == type;
+    return this.peek().type === type;
   }
 
   private peek() {
@@ -167,6 +203,28 @@ class Parser {
       }
     }
     return false;
+  }
+
+  private synchronize() {
+    this.advance();
+
+    while (!this.isAtEnd()) {
+      if (this.previous().type == TokenType.SEMICOLON) return;
+
+      switch (this.peek().type) {
+        case TokenType.CLASS:
+        case TokenType.FUN:
+        case TokenType.VAR:
+        case TokenType.FOR:
+        case TokenType.IF:
+        case TokenType.WHILE:
+        case TokenType.PRINT:
+        case TokenType.RETURN:
+          return;
+      }
+
+      this.advance();
+    }
   }
 }
 export default Parser;
