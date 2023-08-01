8722ba7bef44a8b91c79d92cb98132b02a8a5199
Ken Fowler <development@kennyf.ca>
2023-08-01 19:49:06 -0400

    feat: test if else

---
+++ src/__tests__/interpreter/if/if.test.ts
@@ -0,0 +1,27 @@
+import { matchPrintOutput } from '../../../tool/test.util';
+
+describe('Interpreter Class', () => {
+  describe('if/else', () => {
+    const folder = 'if';
+
+    beforeEach(() => {
+      jest.resetAllMocks();
+    });
+
+    test('executes an if statement', () => {
+      matchPrintOutput(folder, 'if.lox', ['good', 'block', 'true']);
+    });
+
+    test('executes an else statement', () => {
+      matchPrintOutput(folder, 'else.lox', ['good', 'good', 'block']);
+    });
+
+    test('evaluates conditions as expected', () => {
+      matchPrintOutput(folder, 'truth.lox', ['false', 'nil', 'true', '0', 'empty']);
+    });
+
+    test('executes statement with dangling else correctly', () => {
+      matchPrintOutput(folder, 'dangling_else.lox', ['good']);
+    });
+  });
+});
